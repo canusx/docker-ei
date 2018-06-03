01a06f828b84f75006551609fbb3c0a69d889f1c
mustafa <mustafa@genomize.com>
2018-06-03 23:57:41 +0300

    docker end

--- docker-export.py
+++ docker-export.py
@@ -2,5 +2,7 @@ import sh
 
 data = sh.docker('ps')
 docker_list = data.split('\n')[1].split(' ')[0].strip()
+print(sh.docker('stop',docker_list))
 print(sh.docker('cp',docker_list+":/root/.android/avd/","/home/ubuntu/dockeravd"))
+print(sh.tar('zcvf',"/home/ubuntu/dockeravd.tar.gz","/home/ubuntu/dockeravd"))
 print(sh.tar('zcvf',"/home/ubuntu/dockeravd.tar.gz","/home/ubuntu/dockeravd"))
