c4f6423450e3f25c4e23991b8cdeeaa3f6385195
mustafa <mustafa@genomize.com>
2018-06-04 00:31:56 +0300

    import script

---
+++ docker-import.py
@@ -0,0 +1,7 @@
+import sh
+
+data = sh.docker('ps')
+docker_list = data.split('\n')[1].split(' ')[0].strip()
+print(sh.docker('stop',docker_list))
+print(sh.tar('zxf',"/home/ubuntu/dockeravd.tar.gz"))
+print(sh.docker('cp',"/home/ubuntu/dockeravd",docker_list+":/root/.android/avd/"))
