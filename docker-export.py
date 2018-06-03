a32681f408fa8cf110dae47b921f65b0bbc5579c
mustafa <mustafa@genomize.com>
2018-06-03 23:40:06 +0300

    change export to avd

--- docker-export.py
+++ docker-export.py
@@ -2,4 +2,4 @@ import sh
 
 data = sh.docker('ps')
 docker_list = data.split('\n')[1].split(' ')[0].strip()
-sh.docker('export',docker_list,"-o backup.tar.gz")
+sh.docker('cp',docker_list+":/root/.android/avd/ /home/ubuntu/dockeravd")
