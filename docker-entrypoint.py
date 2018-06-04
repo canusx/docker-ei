f784a8d8211417a3d9dbdba7d1e5c844d1825fce
mustafa <mustafa@genomize.com>
2018-06-04 03:34:46 +0300

    docker start

---
+++ docker-entrypoint.py
@@ -0,0 +1,7 @@
+import sh
+
+data = sh.docker('ps')
+docker_list = data.split('\n')[1].split(' ')[0].strip()
+print(sh.docker('stop',docker_list))
+print(sh.docker('cp',"/home/ubuntu/home/ubuntu/entrypoint.sh",docker_list+":/"))
+print(sh.docker('start',docker_list))
