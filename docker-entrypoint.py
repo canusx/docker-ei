b0cdd3f512e82b60d125ccc2a671bc9118a3da5e
mustafa <mustafa@genomize.com>
2018-06-04 04:46:19 +0300

    docker start

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -1,7 +1,6 @@
 import sh
 
 data = sh.docker('ps')
-docker_list = data.split('\n')[1].split(' ')[0].strip()
-print(sh.docker('stop',docker_list))
-print(sh.docker('cp',"/home/ubuntu/entrypoint.sh",docker_list+":/"))
-print(sh.docker('start',docker_list))
+print(sh.docker('stop',"android"))
+print(sh.docker('cp',"/home/ubuntu/entrypoint.sh","android:/"))
+print(sh.docker('start',"android"))
